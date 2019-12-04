a2b58180f3f62f1ae932f1291146e147e2d2d4a3
a8568730 <a8568730@hotmail.com>
2019-12-04 17:23:15 +0800

    * periodic_task pass parameter with .s()

--- periodic_task/periodic_task.py
+++ periodic_task/periodic_task.py
@@ -6,10 +6,17 @@ app = Celery()
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-  print('setup_periodic_tasks!!!!!!!')
   sender.add_periodic_task(10.0, simple_print, name='add every 10 second')
+  # pass parameter with .s()
+  sender.add_periodic_task(10.0, print_name.s('Tshuà Bûn-lī'), name='call name every 10 second')
+
 
 
 @app.task
 def simple_print():
   print('PERIODICALLY SAY HI')
+
+
+@app.task
+def print_name(name):
+  print('PERIODCALLY CALL ' + name)
