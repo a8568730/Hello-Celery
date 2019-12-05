b5bd093b95ebefd12fdcd2e49cad347e118162a8
a8568730 <a8568730@hotmail.com>
2019-12-05 16:29:45 +0800

    + django_autodiscover/

---
+++ django_autodiscover/my_proj/my_proj/celery.py
@@ -0,0 +1,18 @@
+import os
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_proj.settings')
+
+app = Celery('my_proj')
+app.config_from_object('my_proj.celeryconfig')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+      10.0, simple_print.s('hello'), name='print hello every 10'
+    )
